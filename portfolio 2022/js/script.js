718f3d4c891f2642bb3c337ce8e15d97d0e29de9
EToutaiN1 <toutain.ewan@gmail.com>
2022-10-09 17:01:57 +0200

    modified:   portfolio 2022/css/styles.css 	modified:   portfolio 2022/css/styles.css.map 	modified:   portfolio 2022/css/styles.scss 	modified:   portfolio 2022/index.html 	modified:   portfolio 2022/js/script.js

--- portfolio 2022/js/script.js
+++ portfolio 2022/js/script.js
@@ -1,3 +1,22 @@
+let LoaderPage;
+let hidePage;
+
+function myFunction() {
+    LoaderPage = setTimeout(showPage, 2800);
+    hidePage = setTimeout(hideLoader, 4000);
+}
+
+function showPage() {
+    document.querySelector(".loader-animation").style.opacity = "0";
+    document.querySelector(".blockBg-loader").style.animation = "scaleYToZero 500ms 1 ease 700ms";
+    document.querySelector(".blockBg-loader.green").style.animation = "scaleYToZero 500ms 1 ease 800ms";
+}
+
+function hideLoader() {
+    document.querySelector(".loader-animation").style.display = "none";
+    document.querySelector(".loader-wrapper").style.display = "none";
+}
+
 const boxes = document.querySelectorAll(".box");
 
 window.addEventListener("scroll", checkBoxes);
@@ -16,4 +35,43 @@ function checkBoxes() {
       box.classList.remove("show");
     }
   });
+}
+
+
+function mainBtnHover() {
+  const mainBtn = document.querySelectorAll(".main-btn");
+  const mainBtnImg = document.querySelectorAll(".img-btn");
+
+  mainBtn.forEach((btn) => {
+    btn.addEventListener('mouseover', () => { 
+      mainBtnImg.forEach((img) => {
+        img.classList.add('active');
+      })
+    });
+  
+    btn.addEventListener('mouseleave', () => {
+      mainBtnImg.forEach((img) => {
+        img.classList.remove('active');
+      })
+    });
+  })
+}
+
+function secondBtnHover() {
+  const secondBtn = document.querySelectorAll(".secondary-btn");
+  const secondBtnImg = document.querySelectorAll(".secondBtn-img");
+
+  secondBtn.forEach((btn) => {
+    btn.addEventListener('mouseover', () => { 
+      secondBtnImg.forEach((img) => {
+        img.classList.add('active');
+      })
+    });
+  
+    btn.addEventListener('mouseleave', () => {
+      secondBtnImg.forEach((img) => {
+        img.classList.remove('active');
+      })
+    });
+  })
 }
