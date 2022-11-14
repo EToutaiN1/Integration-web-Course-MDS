e02bd45e140034a122a4a392e721e0f3df080ba5
EToutaiN1 <toutain.ewan@gmail.com>
2022-11-14 18:37:49 +0100

    modified:   Chess game/css/_media-queries.scss 	new file:   Chess game/css/_page.scss 	modified:   Chess game/css/style.css 	modified:   Chess game/css/style.css.map 	modified:   Chess game/css/style.scss 	new file:   Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 10.aep 	new file:   Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 11.aep 	deleted:    Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 4.aep 	new file:   Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 7.aep 	renamed:    Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 6.aep -> Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 8.aep 	renamed:    Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 5.aep -> Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 9.aep 	modified:   Chess game/file assets/animation chess game.aep 	new file:   Chess game/file assets/animation chess game_AME/tmpAEtoAMEProject-loader Too Do 1080x1920.aep 	renamed:    Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 2.aep -> Chess game/file assets/animation chess game_AME/tmpAEtoAMEProject-render loader 1080x1920.aep 	renamed:    Chess game/file assets/Adobe After Effects Auto-Save/animation chess game auto-save 3.aep -> Chess game/file assets/animation chess game_AME/tmpAEtoAMEProject-render loader 1080x1920_1.aep 	modified:   Chess game/img/loader-chess-game.json 	new file:   Chess game/img/shadow-Bishop.svg 	new file:   Chess game/img/shadow-Brook.svg 	new file:   Chess game/img/shadow-King.svg 	new file:   Chess game/img/shadow-Knight.svg 	new file:   Chess game/img/shadow-Pawn.svg 	new file:   Chess game/img/shadow-Queen.svg 	modified:   Chess game/index.html 	modified:   Chess game/js/script.js 	new file:   Chess game/page/theme.html

--- Chess game/js/script.js
+++ Chess game/js/script.js
@@ -16,6 +16,44 @@ function hideLoader() {
     document.querySelector(".loader-wrapper").style.display = "none";
 }
 
+const pieceWrapper = document.querySelectorAll('.piece-wrapper');
+
+const blueShadow = document.querySelectorAll('.blue');
+const redShadow = document.querySelectorAll('.red');
+
+console.log(blueShadow);
+
+pieceWrapper[0].addEventListener('mouseover', () => {
+    for (let i = 0; i < blueShadow.length; i++) {
+        blueShadow[i].style.transitionDelay = `${i}00ms`;
+        blueShadow[i].style.transform = 'translate(20%, -20%)'
+        blueShadow[i].style.opacity = '1'
+    }
+})
+
+pieceWrapper[1].addEventListener('mouseover', () => {
+    for (let i = 0; i < redShadow.length; i++) {
+        redShadow[i].style.transitionDelay = `${i}00ms`;
+        redShadow[i].style.transform = 'translate(20%, -20%)'
+        redShadow[i].style.opacity = '1'
+    }
+})
+
+pieceWrapper[0].addEventListener('mouseout', () => {
+    blueShadow.forEach(shadow =>{
+        shadow.style.transform = 'translate(0%, 0%)'
+        shadow.style.opacity = '0'
+    })
+})
+
+pieceWrapper[1].addEventListener('mouseout', () => {
+    redShadow.forEach(shadow =>{
+        shadow.style.transform = 'translate(0%, 0%)'
+        shadow.style.opacity = '0'
+    })
+})
+
+
 const Case = document.querySelectorAll('.box');
 
 // Inserting the Images
